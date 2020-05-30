1de856139a4435d5cc6e64e07f11219c4121e84d
Abhilash K <abhilash.face@gmail.com>
2020-05-30 15:58:15 +0530

    Palindrome

--- Palindrome/solution.js
+++ Palindrome/solution.js
@@ -2,30 +2,6 @@
  * Created by Abhilash K on 5/30/2020.
  */
 
-function findSolution(inputArray)
-{
-    var numtestCases = inputArray[0];
-    var testString = [];
-    var numSymbolsRemoved = [];
-    var splitLineArray = [];
-    var solutionString = "";
-    for(var i=1;i<=numtestCases;i++)
-    {
-        splitLineArray = (inputArray[i]).split(' ');
-        testString[i-1] = splitLineArray[0];
-        numSymbolsRemoved[i-1] = splitLineArray[1];
-    }
-
-    for(var j=0; j<testString.length;j++)
-    {
-        solutionString += "Case #" + String(j +1) + ": ";
-        solutionString += String(IsPalindrome(testString[j], numSymbolsRemoved[j]));
-        solutionString += "\n";
-    }
-
-    return solutionString;
-}
-
 function IsPalindrome(s,i)
 {
     var longestPalindromLength = longestPalindrome(s,0,s.length-1);
@@ -48,4 +24,32 @@ function longestPalindrome(s,i,j)
         return longestPalindrome(s, i + 1, j - 1) + 2;
 
     return Math.max(longestPalindrome(s, i, j - 1), longestPalindrome(s, i + 1, j));
+}
+
+
+
+
+
+function findSolution(inputArray)
+{
+    var numtestCases = inputArray[0];
+    var testString = [];
+    var numSymbolsRemoved = [];
+    var splitLineArray = [];
+    var solutionString = "";
+    for(var i=1;i<=numtestCases;i++)
+    {
+        splitLineArray = (inputArray[i]).split(' ');
+        testString[i-1] = splitLineArray[0];
+        numSymbolsRemoved[i-1] = splitLineArray[1];
+    }
+
+    for(var j=0; j<testString.length;j++)
+    {
+        solutionString += "Case #" + String(j +1) + ": ";
+        solutionString += String(IsPalindrome(testString[j], numSymbolsRemoved[j]));
+        solutionString += "\n";
+    }
+
+    return solutionString;
 }
